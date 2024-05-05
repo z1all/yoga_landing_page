a94816ac1a11067b7b9eacf9113a5a525b5d767c
vitaliy ivanof <ivitalij741@gmail.com>
2024-05-06 02:54:21 +0700

    Adjusting the layout dimensions

--- src/sections/home/SignUpButton/SignUpButton.tsx
+++ src/sections/home/SignUpButton/SignUpButton.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const SignUpButton = () => {
     return (
-        <div style={{position: "absolute"}}>
+        <div style={{position: "absolute", top: "50%", right: "50%"}}>
             SignUpButton
         </div>
     );
--- src/sections/home/Slider/Slider.tsx
+++ src/sections/home/Slider/Slider.tsx
@@ -1,10 +1,38 @@
 import React from 'react';
 import classes from './Slider.module.scss'
+import womanStretching from '../../../assets/imgs/home/woman-stretching-inside.png'
+import womanStandingOneLeg from '../../../assets/imgs/home/smiley-woman-standing-one-leg.png'
+import womanHoldingFoot from '../../../assets/imgs/home/woman-holding-foot.png'
 
 const Slider = () => {
+    const images = [
+        {
+            src: womanStretching,
+            alt: "Woman stretching inside",
+            key: 1,
+        },
+        {
+            src: womanStandingOneLeg,
+            alt: "Smiley woman standing one leg",
+            key: 2,
+        },
+        {
+            src: womanHoldingFoot,
+            alt: "Woman holding foot",
+            key: 3,
+        }
+    ];
+
+    const index = 0;
+
     return (
-        <div className={classes.temp}>
-            Slider
+        <div className={classes.sliderContainer}>
+            <div className={classes.imageContainer}>
+                <img src={images[index].src} alt={images[index].alt} className={classes.image}/>
+            </div>
+            <div className={classes.navigationContainer}>
+                Slider Navigation
+            </div>
         </div>
     );
 };
